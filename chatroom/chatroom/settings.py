d9166bd10d9e0250d370653f519013860057ff84
Arvind Shelke <ucdonor5@gmail.com>
2021-09-14 20:46:52 +0530

    room and ws

--- chatroom/chatroom/settings.py
+++ chatroom/chatroom/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'chat',
     'channels',
+    'chat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
